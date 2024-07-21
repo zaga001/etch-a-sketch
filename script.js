9de3f1b897e074192eb9fd8a34ef1a809631b6de
zaga <163298261+zaga001@users.noreply.github.com>
2024-07-21 11:45:05 +0700

    Complete etch a sketch

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-for (let i = 0; i < 256; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    document.querySelector("#container").appendChild(box);
+function createGrid(size) {
+    const container = document.querySelector("#container");
+    container.innerHTML = "";
+
+    const boxSize = 800 / size;
+
+    for (let i = 0; i < (size * size); i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        box.style.width = `${boxSize}px`;
+        box.style.height = `${boxSize}px`;
+        container.appendChild(box);
+    }
+
+    const boxes = document.querySelectorAll(".box");
+
+    boxes.forEach((box) => {
+        box.addEventListener("mouseover", () => {
+            box.classList.add("newBoxColor");
+        })
+    })
 }
 
-const boxes = document.querySelectorAll(".box");
-
-boxes.forEach((box) => {
-    box.addEventListener("mouseover", () => {
-        box.classList.add("newBoxColor");
-    })
+document.querySelector("#btn").addEventListener("click", () => {
+    let size = prompt("Number of squares per side of the new grid", 16);
+    if (size > 0 && size <= 100) {
+        createGrid(size);
+    } else if (size > 100) {
+        alert("Maximum grid size is 100.");
+    } else {
+        alert("Please enter a valid number.");
+    }
 })
+
+createGrid(16);
